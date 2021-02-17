96a635083c7a3e02b507f3764147cb40fe0f3ecd
Miroslav Strukov <miroslavstrukov3@gmail.com>
2021-02-17 17:54:58 +0200

    fixed minor bug

--- src/components/commentForm/CommentForm.tsx
+++ src/components/commentForm/CommentForm.tsx
@@ -5,7 +5,7 @@ export const CommentForm = (props:any) =>{
     return (
         <div className="card__comment-add">
             <div className="wrapper">
-                <form className="card__comment-form" onSubmit={(e)=>{e.preventDefault(); props.addComment(((e.target as HTMLFormElement).comment.value))}}>
+                <form className="card__comment-form" onKeyPress={(e)=>{if((e.keyCode == 10 || e.keyCode == 13) && e.ctrlKey){document.querySelector('form')?.submit();}}} onSubmit={(e)=>{e.preventDefault(); props.addComment(((e.target as HTMLFormElement).comment.value));(e.target as HTMLFormElement).comment.value='';}}>
                     <textarea name='comment' className='card__input' ></textarea>
                     <input type="submit" className='card__submit-comment' value='Написать консультанту'/>
                 </form>
